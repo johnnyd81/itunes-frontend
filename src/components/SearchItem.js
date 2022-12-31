fa20ba52c596d5142ea6eb6181d481e5c5b33893
Jonathan <johnnydjohnson81@gmail.com>
2022-12-31 17:10:03 +0200

    edited files

--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import '../App.css'
-import axios from 'axios' //I used axios to make my post requests
+import React from "react";
+import "../App.css";
+import axios from "axios"; //I used axios to make my post requests
 
 const SearchItem = ({ item, getFavourites }) => {
   //the function below triggers each time a favourite item is selected
   const addFavourite = () => {
     //the post request is made to the backend that adds the new favourite item to the backend favourites array
     axios
-      .post('/api/addItem', {
+      .post("https://itunes-server.onrender.com/api/addItem", {
         newItem: item,
       })
-      .catch((error) => console.log('Error :', error))
+      .catch((error) => console.log("Error :", error));
     /*the function below gets the data from the backend favourites array 
       and adds it to the frontend favourites state variable*/
-    getFavourites()
-  }
+    getFavourites();
+  };
 
   //the function below handles the onclick event and also notifies the user that an item has been selected
   const handleAdd = (event) => {
-    event.preventDefault()
-    addFavourite()
-    alert('Favourite added')
-  }
+    event.preventDefault();
+    addFavourite();
+    alert("Favourite added");
+  };
 
   /*of the returned search data the image, trackname, artistname, kind and wrappertype are displayed*/
   return (
@@ -61,7 +61,7 @@ const SearchItem = ({ item, getFavourites }) => {
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchItem
+export default SearchItem;
